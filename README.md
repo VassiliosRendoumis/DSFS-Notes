4ffc35db92e636232ce11f1455ca0b74d73d4dd6
Vassilios Rendoumis <vassilios.rendoumis@gmail.com>
2016-05-08 02:18:26 +0300

    Addition to my nodes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # DSFS-Notes
 
-Some additonal code on the book Data Science from Scratch
+Some additional code and notes on the book Data Science from Scratch
+
+The original code by Joel Grus can be forked/downloaded from its respective gitHub account.
+https://github.com/joelgrus/data-science-from-scratch
--- chapter_1.py
+++ chapter_1.py
@@ -4,7 +4,8 @@ Created on Mon May 02 22:13:49 2016
 
 """
 from __future__ import division
-from collections import Counter
+from collections import Counter, defaultdict
+import itertools as itr
 
 users = [
             {"id":0, "name":"Hero"},
@@ -37,7 +38,7 @@ def print_friendships():
         print fs
 
 # Let's add another field to every user dictionaryin the users list.
-# This new field is the user's friends list and it will be a list.
+# This new field is the user's friends list and it is ... a list.
 # First we add an empty list of friends to each user.
     
 for user in users:
@@ -138,4 +139,90 @@ def friends_of_friend_ids(user):
 # The result should be Counter({0: 2, 5: 1})
 # That is, user Chi has two common friends with user Hero (id 0)
 # and one common friend with user Clive (id 5)
+
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+def data_scientists_who_like(target_interest):
+    """Returns users with certain interest"""
+    return [user_id 
+            for user_id, user_interest in interests 
+            if user_interest == target_interest]
+                
+# Let's build an index from interests to users:                
+# keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+#print user_ids_by_interest
+
+# Although, defaultdicts belong to the high performance containers family in Python,
+# you can use also a common dictionary, like so:
+my_user_ids_by_interest = {j:[] for i, j in interests}
+
+for k, v in my_user_ids_by_interest.iteritems():
+    for user_id, interest in interests:
+        if interest == k:
+            my_user_ids_by_interest[k].append(user_id)
             
+# print my_user_ids_by_interest
+            
+# Let's build also an index from users to interests:
+# Keys are user_ids, values are lists of interests for that user_id
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+#print interests_by_user_id
+
+def most_common_interests_with(user_id):
+    return Counter(interested_user_id
+                    for interest in  interests_by_user_id[user_id]
+                    for interested_user_id in user_ids_by_interest[interest]
+                    if interested_user_id != user_id)
+
+# Let's see who has common interests with user Hero (id 0) and what's their 
+# number
+
+#print most_common_interests_with(0)
+
+# If we want to see how many interests in common user(s) have with a given user
+# we can check the maximum number of common interests among users with 
+# common interests
+# print max([v for k, v in most_common_interests_with(0).items() ])
+
+def pretty_printing_users_with_common_interests(user_id):
+    """Prints out a report providing which user(s) have the maximum number of
+       common interests, with the given user (user is given via user_id)
+    """
+
+    max_common_interests = max([v for k, v in most_common_interests_with(user_id).iteritems()])
+    users_with_max_common_interests = []
+    for user, interest_num in most_common_interests_with(user_id).iteritems():
+        if interest_num == max_common_interests:
+            users_with_max_common_interests.append(user)
+    print "User", users[user_id]["name"], "has", max_common_interests, \
+          " interest(s) in common with user(s)", \
+          ''.join([''.join([users[_]["name"], ", "]) for _ in users_with_max_common_interests])       
+    
+
+
+pretty_printing_users_with_common_interests(9)
